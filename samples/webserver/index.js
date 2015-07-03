68100dbfba5e44ac40afcc41c6d3e4ed1f343c92
Takaki Makino <t-github@snowelm.com>
2015-07-03 06:02:03 -0700

    First version of URL popper

--- samples/webserver/index.js
+++ samples/webserver/index.js
@@ -1,15 +1,8 @@
-onload = function() {
-  var start = document.getElementById("start");
-  var stop = document.getElementById("stop");
-  var hosts = document.getElementById("hosts");
-  var port = document.getElementById("port");
-  var directory = document.getElementById("directory");
-
+var onload = function() {
   var tcpServer = chrome.sockets.tcpServer;
   var tcpSocket = chrome.sockets.tcp;
 
   var serverSocketId = null;
-  var filesMap = {};
 
   var stringToUint8Array = function(string) {
     var buffer = new ArrayBuffer(string.length);
@@ -27,11 +20,6 @@ onload = function() {
       str += String.fromCharCode(uArrayVal[s]);
     }
     return str;
-  };
-
-  var logToScreen = function(log) {
-    logger.textContent += log + "\n";
-    logger.scrollTop = logger.scrollHeight;
   };
 
   var destroySocketById = function(socketId) {
@@ -53,70 +41,31 @@ onload = function() {
     tcpSocket.onReceive.removeListener(onReceive);
   };
 
-  var sendReplyToSocket = function(socketId, buffer, keepAlive) {
+  var sendReplyToSocket = function(socketId, buffer) {
+    console.log(arrayBufferToString(buffer));
     // verify that socket is still connected before trying to send data
     tcpSocket.getInfo(socketId, function(socketInfo) {
       if (!socketInfo.connected) {
         destroySocketById(socketId);
         return;
       }
-
-      tcpSocket.setKeepAlive(socketId, keepAlive, 1, function() {
-        if (!chrome.runtime.lastError) {
-          tcpSocket.send(socketId, buffer, function(writeInfo) {
-            console.log("WRITE", writeInfo);
-
-            if (!keepAlive || chrome.runtime.lastError) {
-              destroySocketById(socketId);
-            }
-          });
-        }
-        else {
-          console.warn("chrome.sockets.tcp.setKeepAlive:", chrome.runtime.lastError);
-          destroySocketById(socketId);
-        }
+      tcpSocket.send(socketId, buffer, function(writeInfo) {
+        console.log("WRITE", writeInfo);
+        destroySocketById(socketId);
       });
     });
   };
 
-  var getResponseHeader = function(errorCode) {
+  var getSuccessHeader = function() {
     var httpStatus = "HTTP/1.0 200 OK";
-
-    if (!file || errorCode) {
-      httpStatus = "HTTP/1.0 " + (errorCode || 404) + " Not Found";
-    }
-
     var lines = [
       httpStatus,
-      "Content-length: 0",
+      "Content-length: 8",
       "Content-type: text/plain"
     ];
+    console.log(httpStatus);
 
     return stringToUint8Array(lines.join("\n") + "\n\n");
-  };
-
-  var getErrorHeader = function(errorCode) {
-    return getResponseHeader(errorCode);
-  };
-
-  var getSuccessHeader = function() {
-    return getResponseHeader(null);
-  };
-
-  var writeErrorResponse = function(socketId, errorCode) {
-    console.info("writeErrorResponse:: begin... ");
-
-    var header = getErrorHeader(errorCode);
-    console.info("writeErrorResponse:: Done setting header...");
-    var outputBuffer = new ArrayBuffer(header.byteLength);
-    var view = new Uint8Array(outputBuffer);
-    view.set(header, 0);
-    console.info("writeErrorResponse:: Done setting view...");
-
-    sendReplyToSocket(socketId, outputBuffer);
-
-    console.info("writeErrorResponse::filereader:: end onload...");
-    console.info("writeErrorResponse:: end...");
   };
 
   var write200Response = function(socketId) {
@@ -125,7 +74,7 @@ onload = function() {
     var view = new Uint8Array(outputBuffer);
     view.set(header, 0);
     view.set(stringToUint8Array('Success!'), header.byteLength);
-    sendReplyToSocket(socketId, outputBuffer, keepAlive);
+    sendReplyToSocket(socketId, outputBuffer);
   };
 
   var onAccept = function(acceptInfo) {
@@ -155,28 +104,25 @@ onload = function() {
     var uri = data.substring(4, uriEnd);
     
     chrome.browser.openTab({url: uri}, function(){
-       logToScreen("GET 200 " + uri);
+       console.log("GET 200 " + uri);
        write200Response(socketId);
     });
   };
 
-  start_func = function() {
     tcpServer.create({}, function(socketInfo) {
       serverSocketId = socketInfo.socketId;
 
-      tcpServer.listen(serverSocketId, "localhost", 28282, 50, function(result) {
+      tcpServer.listen(serverSocketId, "127.0.0.1", 28283, 50, function(result) {
         console.log("LISTENING:", result);
 
         tcpServer.onAccept.addListener(onAccept);
         tcpSocket.onReceive.addListener(onReceive);
       });
     });
-
-  };
 };
 
 chrome.app.runtime.onLaunched.addListener(function(intentData) {
-  onload().start_func();
+  onload();
 });
 
 
